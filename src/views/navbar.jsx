edbf35c70615483eec0672b1c2dece83e736b5bd
f.staikos7 <f.staikos7@gmail.com>
2023-03-27 11:47:06 +0300

    feat: Added navbar

---
+++ src/views/navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/profile">Profile</NavLink>
+        </li>
+        <li>
+          <NavLink to="/goals">Goals</NavLink>
+        </li>
+        <li>
+          <NavLink to="/workouts">Workouts</NavLink>
+        </li>
+        <li>
+          <NavLink to="/exercises">Exercises</NavLink>
+        </li>
+        <li>
+          <NavLink to="/programs">Programs</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
