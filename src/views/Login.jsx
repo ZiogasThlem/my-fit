99706a907e14acfa0d80a45f022284902282ef37
ZiogasThlem <ziogas.thlem@gmail.com>
2023-03-27 23:29:29 +0300

    button styling

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,4 +1,7 @@
+import { useEffect } from "react";
 import keycloak from "../keycloak"
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 //check if user exists
 //if not register the user
@@ -11,6 +14,19 @@ const Login = () =>{
       const handleLogin = async () => {
         keycloak.login()
       }
+
+      const navigate = useNavigate()
+      const [username, setUsername] = useState('')
+
+      // useEffect((
+      //   () => {
+      //     if (keycloak.authenticated) 
+      //       setUsername(keycloak.tokenParsed.preffered_username)
+      //       navigate('/programs')
+      //   }
+      // ),[username])
+
+
       const handleRegister = () => {
         keycloak.register()
       }
@@ -22,13 +38,12 @@ const Login = () =>{
 
     
       return (
-        <>
-        
-          <button onClick={handleLogin}>Login</button>
-          <button onClick={handleRegister}>Register</button>
-          <button onClick={handleToken}>Test</button>
-
-        </>
+        <div className="login-btn-grp">
+          <h1>Welcome!</h1>
+          <button onClick={handleLogin}  className="login-btn">Login</button>
+          <button onClick={handleRegister} className="login-btn">Register</button>
+          <button onClick={handleToken} className="login-btn">Test</button>
+        </div>
       )
     }
 
--- src/views/navbar.jsx
+++ src/views/navbar.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useEffect, useState } from "react"
+import keycloak from "../keycloak"
 
 const Navbar = () => {
+
+  const [username , setUsername] = useState('')
+
+  useEffect(()=>{
+    try {
+      if(username!=null){
+         setUsername(keycloak.tokenParsed.preferred_username)
+      }
+    } catch (error) {
+    }
+},[username])
+
+  const handleLogout = () => keycloak.logout()
+
   return (
     <nav>
-      
-      <ul>
       <img className='logo' src="../mefitlogo.png" alt="logo" width={50} height={50} />
+      {username!='' && 
+      <ul>
         <li>
         
           <NavLink to="/profile">Profile</NavLink>
@@ -23,7 +39,10 @@ const Navbar = () => {
         <li>
           <NavLink to="/programs">Programs</NavLink>
         </li>
-      </ul>
+        <li>
+          <button onClick={handleLogout}>Logout</button>
+        </li>
+      </ul>}
     </nav>
   );
 };
