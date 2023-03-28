7785ef68089db4f67f2fe1a0bb4f49945f663377
f.staikos7 <f.staikos7@gmail.com>
2023-03-28 23:27:10 +0300

    fix

--- src/reduxParts/store.js
+++ src/reduxParts/store.js
@@ -2,7 +2,7 @@ import { configureStore} from "@reduxjs/toolkit";
 import exerciseReducer from './reducers/exerciseSlice.js';
 import thunkMiddleWare from "redux-thunk";
 import { applyMiddleware } from "@reduxjs/toolkit";
-//import logger from 'redux-logger';
+import logger from 'redux-logger';
 import { composeWithDevTools } from "redux-devtools-extension";
 // import { composeWithDevTools } from "@reduxjs/toolkit/dist/devtoolsExtension";
 import workoutReducer from "./reducers/workoutSlice.js"
