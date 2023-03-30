961ac5a9424d25f6fe719443574d72b1ea9dd1db
Fotis Staikos <64866131+NotFotis@users.noreply.github.com>
2023-03-30 16:27:50 +0300

    Update navbar.jsx

--- src/views/navbar.jsx
+++ src/views/navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
     }
 },[username])
 
-  const handleLogout = () => keycloak.logout({ redirectUri: ['https://my-fit-one.vercel.app/login', 'https://*.vercel.app/login' ,'http://localhost:3000/login' ]})
+  const handleLogout = () => keycloak.logout({ redirectUri: 'https://my-fit-one.vercel.app/login'})
 
 
   return (
