03775ce17bf4bc10fa170f6b940e07729b5d40d4
f.staikos7 <f.staikos7@gmail.com>
2023-03-30 13:09:02 +0300

    feat: added redirect on login

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -4,49 +4,38 @@ import { Redirect } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
-//check if user exists
-//if not register the user
-//set an initial state for user after login or register
+const Login = () => {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState('');
 
-
-
-const Login = () =>{
-    
-      const handleLogin = async () => {
-        keycloak.login()        	
-      
-      }
-
-      const navigate = useNavigate()
-      const [username, setUsername] = useState('')
-
-      // useEffect((
-      //   () => {
-      //     if (keycloak.authenticated) 
-      //       setUsername(keycloak.tokenParsed.preffered_username)
-      //       navigate('/programs')
-      //   }
-      // ),[username])
-
-
-      const handleRegister = () => {
-        keycloak.register()
-      }
-
-      const handleToken = () => {
-        console.log(keycloak.tokenParsed);
-        console.log(keycloak.token)
-      }
-
-    
-      return (
-        <div className="login-btn-grp">
-          <h1>Welcome!</h1>
-          <button onClick={handleLogin}  className="login-btn">Login</button>          
-          <button onClick={handleRegister} className="login-btn">Register</button>
-          <button onClick={handleToken} className="login-btn">Test</button>
-        </div>
-      )
+  useEffect(() => {
+    if (keycloak.authenticated) {
+      setUsername(keycloak.tokenParsed.preferred_username);
+      navigate('./profile');
     }
+  }, [username]);
 
-export default Login
+  const handleLogin = async () => {
+    keycloak.login({ redirectUri: '/profile' });
+  };
+
+  const handleRegister = () => {
+    keycloak.register();
+  };
+
+  const handleToken = () => {
+    console.log(keycloak.tokenParsed);
+    console.log(keycloak.token);
+  };
+
+  return (
+    <div className="login-btn-grp">
+      <h1>Welcome!</h1>
+      <button onClick={handleLogin} className="login-btn">Login</button>
+      <button onClick={handleRegister} className="login-btn">Register</button>
+      <button onClick={handleToken} className="login-btn">Test</button>
+    </div>
+  );
+};
+
+export default Login;
