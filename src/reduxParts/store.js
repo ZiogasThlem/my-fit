0a18762935813ee373314193a01e80441c5c2157
f.staikos7 <f.staikos7@gmail.com>
2023-03-31 10:26:07 +0300

    assas

--- src/reduxParts/store.js
+++ src/reduxParts/store.js
@@ -1,27 +1,21 @@
-
 import { configureStore} from "@reduxjs/toolkit";
-import exerciseReducer from './reducers/exerciseSlice.js';
+import exerciseReducer from './exercise/exerciseSlice.js';
 import thunkMiddleWare from "redux-thunk";
 import { applyMiddleware } from "@reduxjs/toolkit";
-import logger from 'redux-logger';
+//.import logger from 'redux-logger';
 import { composeWithDevTools } from "redux-devtools-extension";
 // import { composeWithDevTools } from "@reduxjs/toolkit/dist/devtoolsExtension";
-import workoutReducer from "./reducers/workoutSlice.js"
-import goalReducer from "./reducers/goalSlice.js"
-import programReducer from "./reducers/programSlice.js"
+
 
 const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleWare))
 
 export default configureStore({
 
     reducer: {
-        exercise: exerciseReducer,
-        workout:workoutReducer,
-        program:programReducer,
-        goal:goalReducer
+        exercise: exerciseReducer
+        
     },
     
     // middleware:(getDefaultMiddleware)=>getDefaultMiddleware().concat(logger),
     // composedEnhancer
 })
-
